0a60ab7d73fb7d96cf0983255ce59c0b1b8b5292
Karthik Nandakumar <117186861+kayteekay1412@users.noreply.github.com>
2024-07-15 22:24:30 +0530

    Update AQI.py

--- AQI.py
+++ AQI.py
@@ -8,7 +8,7 @@ import plotly.io as pio
 import plotly.graph_objects as go
 pio.templates.default = "plotly_white"
 
-data = pd.read_csv(r"D:\Karthik\Python Programs\Air Quality Index\delhiaqi (2).csv")
+data = pd.read_csv(r"delhiaqi (2).csv")
 print(data.head())
 
 data['date'] = pd.to_datetime(data['date'])
@@ -17,20 +17,12 @@ print(data.describe())
 # time series plot for each air pollutant
 fig = go.Figure()
 
-for pollutant in ['co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3']:
-    fig.add_trace(go.Scatter(x=data['date'], y=data[pollutant], mode='lines', 
-                             name=pollutant)) #We are just plotting trace of multiple scatter plots in 1 figure
-#name is key of figure
-fig.update_layout(title='Time Series Analysis of Air Pollutants in Delhi',
-                  xaxis_title='Date', yaxis_title='Concentration (µg/m³)')
+for pollutant in ['co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3']:fig.add_trace(go.Scatter(x=data['date'], y=data[pollutant], mode='lines', name=pollutant))
+fig.update_layout(title='Time Series Analysis of Air Pollutants in Delhi',xaxis_title='Date', yaxis_title='Concentration (µg/m³)')
 fig.show()
 
 # Define AQI breakpoints and corresponding AQI values
-aqi_breakpoints = [
-    (0, 12.0, 50), (12.1, 35.4, 100), (35.5, 55.4, 150),
-    (55.5, 150.4, 200), (150.5, 250.4, 300), (250.5, 350.4, 400),
-    (350.5, 500.4, 500)
-]
+aqi_breakpoints = [(0, 12.0, 50), (12.1, 35.4, 100), (35.5, 55.4, 150),(55.5, 150.4, 200), (150.5, 250.4, 300), (250.5, 350.4, 400),(350.5, 500.4, 500)]
 
 def calculate_aqi(pollutant_name, concentration):
     for low, high, aqi in aqi_breakpoints:
@@ -52,9 +44,7 @@ data['AQI'] = data.apply(calculate_overall_aqi, axis=1)
 
 # Define AQI categories
 aqi_categories = [
-    (0, 50, 'Good'), (51, 100, 'Moderate'), (101, 150, 'Unhealthy for Sensitive Groups'),
-    (151, 200, 'Unhealthy'), (201, 300, 'Very Unhealthy'), (301, 500, 'Hazardous')
-]
+    (0, 50, 'Good'), (51, 100, 'Moderate'), (101, 150, 'Unhealthy for Sensitive Groups'),(151, 200, 'Unhealthy'), (201, 300, 'Very Unhealthy'), (301, 500, 'Hazardous')]
 
 def categorize_aqi(aqi_value):
     for low, high, category in aqi_categories:
@@ -72,10 +62,7 @@ fig = px.bar(data, x="date", y="AQI",
 fig.update_xaxes(title="Date")
 fig.update_yaxes(title="AQI")
 fig.show()
-
-fig = px.histogram(data, x="date", 
-                    color="AQI Category", 
-                    title="AQI Category Distribution Over Time")
+fig = px.histogram(data, x="date", color="AQI Category", title="AQI Category Distribution Over Time")
 fig.update_xaxes(title="Date")
 fig.update_yaxes(title="Count")
 fig.show()
@@ -87,15 +74,10 @@ pollutant_colors = px.colors.qualitative.Plotly
 # Calculate the sum of pollutant concentrations
 total_concentrations = data[pollutants].sum()
 # Create a DataFrame for the concentrations
-concentration_data = pd.DataFrame({
-    "Pollutant": pollutants,
-    "Concentration": total_concentrations
-})
+concentration_data = pd.DataFrame({"Pollutant": pollutants, "Concentration": total_concentrations})
 
 # Create a donut plot for pollutant concentrations
-fig =px.pie(concentration_data, names="Pollutant", values="Concentration",
-             title="Pollutant Concentrations in Delhi"
-            , color_discrete_sequence=pollutant_colors)
+fig =px.pie(concentration_data, names="Pollutant", values="Concentration",title="Pollutant Concentrations in Delhi", color_discrete_sequence=pollutant_colors)
 
 # Update layout for the pie chart
 fig.update_traces(textinfo="percent+label")
@@ -112,8 +94,7 @@ data['Hour'] = pd.to_datetime(data['date']).dt.hour
 hourly_avg_aqi = data.groupby('Hour')['AQI'].mean().reset_index()
 
 # Create a line plot for hourly trends in AQI
-fig = px.line(hourly_avg_aqi, x='Hour', y='AQI', 
-              title='Hourly Average AQI Trends in Delhi (Jan 2023)')
+fig = px.line(hourly_avg_aqi, x='Hour', y='AQI', title='Hourly Average AQI Trends in Delhi (Jan 2023)')
 fig.update_xaxes(title="Hour of the Day")
 fig.update_yaxes(title="Average AQI")
 fig.show()
@@ -121,8 +102,7 @@ fig.show()
 # Average AQI by Day of the Week
 data['Day_of_Week'] = data['date'].dt.day_name()
 average_aqi_by_day = data.groupby('Day_of_Week')['AQI'].mean().reindex(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-fig = px.bar(average_aqi_by_day, x=average_aqi_by_day.index, y='AQI', 
-              title='Average AQI by Day of the Week')
+fig = px.bar(average_aqi_by_day, x=average_aqi_by_day.index, y='AQI', title='Average AQI by Day of the Week')
 fig.update_xaxes(title="Day of the Week")
 fig.update_yaxes(title="Average AQI")
 fig.show()
